2d122724067df3af74c114a26933a9d41fd4b0e2
Sakib Fakir <FakirSakib22232@gmail.com>
2025-09-20 12:49:41 +0600

    start linter learning

---
+++ Readme.md
@@ -0,0 +1,14 @@
+<!-- install eslint -> npm install eslint -->
+<!-- init eslint config file -> npx eslint --init -->
+<!-- than  set rules based on coding style -->
+
+<!--  pretter -->
+<!-- rules for pretteir  -->
+
+{
+"semi": true, /// gave semicon every variable complete
+"singleQuote": true, /// gave single quote every " " ;
+"trailingComma": "es5", //// gave comma ,
+
+"tabWidth": 2 ; /// in function or tab gave 2 tab space
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+// no prettier
+// let x = 10
+// after pretter
+// let x = 10;
+
+// no prettier
+// let name ="sakib"
+
+// after prettier
+let name = 'sakib';
+
+// no preffter
+const obj = {
+  a: 1,
+  b: 10
+};
+// after pretter
+// const obj = {
+//   a: 1,
+//   b: 10
+// };
+
+
+// no prettier
+// function addNumber(a, b) {
+// console.log(a + b);
+// }
+// after prettier
+// function addNumber(a, b) {
+//  console.log(a + b);
+// }
